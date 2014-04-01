4fa575656f9cbf8c50db6da581354fc73f2d2f34
Frank <frankhn0801@gmail.com>
2014-04-01 13:58:57 -0600

    adding angular files

---
+++ src/FireTower.Presentation/App/config.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('firetower', ['ng', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: 'App/Views/index.html',
+            controller: ''
+        })
+        otherwise({
+            templateUrl: ''
+        });
+});
---
+++ src/FireTower.Presentation/Modules/HomeModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FireTower.Presentation.Modules
+{
+    public class HomeModule : NancyModule
+    {
+    }
+}
