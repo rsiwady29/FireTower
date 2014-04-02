70135535a0d9d845bf1b58515699eb35aa3b8bc1
Richard Siwady <richard@acklenavenue.com>
2014-04-01 19:01:14 -0600

    removed dummy controllers

--- src/FireTower.Presentation/App/config.js
+++ src/FireTower.Presentation/App/config.js
@@ -1,7 +1,7 @@
 ﻿var app = angular.module('firetower', ['ionic']);
 
 
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('Login', {
             url: "/login",
@@ -15,52 +15,4 @@ app.config(function ($stateProvider, $urlRouterProvider) {
         });
 
     $urlRouterProvider.otherwise('/fuegos_reportados');
-})
-.controller('MainCtrl', function ($scope, $ionicSideMenuDelegate) {
-    $scope.attendees = [
-      { firstname: 'Nicolas', lastname: 'Cage' },
-      { firstname: 'Jean-Claude', lastname: 'Van Damme' },
-      { firstname: 'Keanu', lastname: 'Reeves' },
-      { firstname: 'Steven', lastname: 'Seagal' }
-    ];
-
-    $scope.toggleLeft = function () {
-        $ionicSideMenuDelegate.toggleLeft();
-    };
-})
-
-.controller('CheckinCtrl', function ($scope) {
-    $scope.showForm = true;
-
-    $scope.shirtSizes = [
-      { text: 'Large', value: 'L' },
-      { text: 'Medium', value: 'M' },
-      { text: 'Small', value: 'S' }
-    ];
-
-    $scope.attendee = {};
-    $scope.submit = function () {
-        if (!$scope.attendee.firstname) {
-            alert('Info required');
-            return;
-        }
-        $scope.showForm = false;
-        $scope.attendees.push($scope.attendee);
-    };
-
-})
-
-.controller('AttendeesCtrl', function ($scope) {
-
-    $scope.activity = [];
-    $scope.arrivedChange = function (attendee) {
-        var msg = attendee.firstname + ' ' + attendee.lastname;
-        msg += (!attendee.arrived ? ' has arrived, ' : ' just left, ');
-        msg += new Date().getMilliseconds();
-        $scope.activity.push(msg);
-        if ($scope.activity.length > 3) {
-            $scope.activity.splice(0, 1);
-        }
-    };
-
 });
