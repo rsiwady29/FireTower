aefe964d4a26bc087af0df7b67eb83a65e45c230
Richard Siwady <richard@acklenavenue.com>
2014-04-02 17:34:45 -0600

    fake data

--- src/FireTower.Presentation/App/controller/reportes.js
+++ src/FireTower.Presentation/App/controller/reportes.js
@@ -1,10 +1,12 @@
 angular.module('firetower')
     .controller('ReportesController', ['$scope', 'data', function($scope, data) {
-        data.getAllReports()
+
+        $scope.reportes = data.getAllReports();
+/*        data.getAllReports()
             .success(function (data) {
                 $scope.reportes = data;
             })
             .error(function(error) {
                 alert(error);
-            });
+            });*/
     }]);
--- src/FireTower.Presentation/App/services/data.js
+++ src/FireTower.Presentation/App/services/data.js
@@ -7,11 +7,43 @@
     var db = 'appharbor_ab50c767-930d-4b7d-9571-dd2a0b62d5a9';
     var collection = 'DisasterViewModel';
         
-    factory.getAllReports = function () {
-        var url = baseUrl + db + '/collections/' + collection + '?apiKey=' + apiKey;
+    factory.getAllReports = function() {
+        return [
+            {
+                "_id": {
+                    "$oid": "123aasd32"
+                },
+                "CreatedDate": "hace 20 minutos",
+                "LocationDescription": "Near Comayagua",
+                "Location": [0.45234234, 0.01231234],
+                "SeverityNotes": [2],
+                "Images": ['http://placehold.it/350x150', 'http://ww4.hdnux.com/photos/12/72/01/2859127/13/628x471.jpg']
+            },
+            {
+                "_id": {
+                    "$oid": "123aasd32"
+                },
+                "CreatedDate": "hace 20 minutos",
+                "LocationDescription": "Near Comayagua",
+                "Location": [0.45234234, 0.01231234],
+                "SeverityNotes": [2],
+                "Images": ['http://placehold.it/350x150', 'https://www.dosomething.org/files/styles/blog_landscape/public/pictures/wildfires8763.jpg?itok=VxL9mcG7']
+            },
+            {
+                "_id": {
+                    "$oid": "123aasd32"
+                },
+                "CreatedDate": "hace 20 minutos",
+                "LocationDescription": "Near Comayagua",
+                "Location": [0.45234234, 0.01231234],
+                "SeverityNotes": [2],
+                "Images": ['http://placehold.it/350x150', 'http://placehold.it/350x450']
+            }
+        ];
+        /*var url = baseUrl + db + '/collections/' + collection + '?apiKey=' + apiKey;
         console.log(url);
         return $http.get(url);
-
+*/
     };
 
     factory.getReportById = function(id) {
