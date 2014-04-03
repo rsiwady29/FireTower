56b1bfbe28a0f3c207bff9d217592d742a6301a4
Javier Suazo <javier@acklenavenue.com>
2014-04-03 14:28:18 -0600

    Adding pic

--- src/FireTower.Presentation/App/www/App/controller/reportes.js
+++ src/FireTower.Presentation/App/www/App/controller/reportes.js
@@ -20,9 +20,34 @@ angular.module('firetower')
                 })
                 .error(function(error) {
 
+<<<<<<< HEAD
                 });
         };
         
         $scope.reportes = null;
         getAllReports();
+=======
+        $scope.reportes = data.getAllReports();
+/*        data.getAllReports()
+            .success(function (data) {
+                $scope.reportes = data;
+            })
+            .error(function(error) {
+                alert(error);
+            });*/
+        $scope.takePicture = function () {
+            navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+                quality: 50,
+                destinationType: destinationType.DATA_URL
+            });
+
+            function onPhotoDataSuccess(imageData) {
+                alert(imageData);
+            }
+            
+            function onFail(message) {
+                alert('Failed because: ' + message);
+            }
+        };
+>>>>>>> Adding pic
     }]);
