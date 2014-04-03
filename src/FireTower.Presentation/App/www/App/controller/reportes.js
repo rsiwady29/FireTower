381e7f176e0874851df8b7b4227bc67071d8e58b
Richard Siwady <richard@acklenavenue.com>
2014-04-03 13:31:41 -0600

    added momentjs to format dates

--- src/FireTower.Presentation/App/www/App/controller/reportes.js
+++ src/FireTower.Presentation/App/www/App/controller/reportes.js
@@ -1,12 +1,20 @@
 angular.module('firetower')
     .controller('ReportesController', ['$scope', 'data', function($scope, data) {
 
-        $scope.reportes = data.getAllReports();
-/*        data.getAllReports()
+        //$scope.reportes = data.getAllReports();
+        data.getAllReports()
             .success(function (data) {
                 $scope.reportes = data;
+
+                for (var i = 0; i < $scope.reportes.length; i++) {
+                    var formattedDate = $scope.reportes[i].CreatedDate.$date;
+                    formattedDate = moment((new Date()).toLocaleDateString()).fromNow();
+                    $scope.reportes[i].CreatedDate.$date = formattedDate;
+                }
+                console.log($scope.reportes);
+
             })
             .error(function(error) {
                 alert(error);
-            });*/
+            });
     }]);
