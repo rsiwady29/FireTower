26baac60a137aad2119a03fb005b136f08049a2b
unknown <fabrizio.dfc2@gmail.com>
2014-04-03 13:37:51 -0600

    Added location selection screen

---
+++ src/FireTower.Presentation/App/config.js
@@ -0,0 +1,54 @@
+﻿angular.module('firetower', ['ionic', 'google-maps'])
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('app', {
+            url: "/app",
+            templateUrl: "App/views/menu.html"
+        })
+        .state('app.inicio', {
+            url: "/inicio",
+            views: {
+                'menuContent': {
+                    templateUrl: "App/views/inicio.html"
+                }
+            }
+        })
+        .state('app.reportes', {
+            url: "/reportes",
+            views: {
+                'menuContent': {
+                    templateUrl: "App/views/reportes.html",
+                    controller: 'ReportesController'
+                }
+            }
+        })
+        .state('app.reporte', {
+            url: "/reporte/:reporteId",
+            views: {
+                'menuContent': {
+                    templateUrl: "App/views/reporte.html",
+                    controller: 'ReporteController'
+                }
+            }
+        })
+        .state('app.newfirelocation', {
+            url: "/newfirelocation",
+            views: {
+                'menuContent': {
+                    templateUrl: "App/views/new-fire-location.html",
+                    controller: 'NewFireLocationController'
+                }
+            }
+        });
+     
+    $urlRouterProvider.otherwise('/app/inicio');
+});
---
+++ src/FireTower.Presentation/App/controller/new-fire-location.js
@@ -0,0 +1,12 @@
+﻿angular.module('firetower')
+    .controller('NewFireLocationController', ['$scope', '$stateParams', 'data', function ($scope, $stateParams, data) {
+        $scope.location = { lat: 15.22, lng: -89.88 };
+
+        $scope.map = {
+            center: {
+                latitude: $scope.location.lat,
+                longitude: $scope.location.lng
+            },
+            zoom: 12
+        };
+    }]);
