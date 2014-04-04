b63827b1343f047398aff9a11786fedae1665a15
unknown <fabrizio.dfc2@gmail.com>
2014-04-03 22:14:19 -0600

    Layout fixes

--- src/FireTower.Presentation/App/controller/new-fire-location.js
+++ src/FireTower.Presentation/App/controller/new-fire-location.js
@@ -1,38 +1,30 @@
 ﻿angular.module('firetower')
-    .controller('NewFireLocationController', ['$scope', '$stateParams', 'data', function ($scope, $stateParams, data) {
+    .controller('NewFireLocationController', ['$scope', function ($scope) {
+
         $scope.location = { latitude: 15.22, longitude: -89.88 };
-        
+
         $scope.map = {
             center: $scope.location,
             zoom: 12
         };
-        
+
         function getCurrentPosition(position) {
             $scope.location = {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             };
-            
+
             $scope.map = {
                 center: $scope.location,
                 zoom: 12
             };
         }
-        
+
         function positionFailure(error) {
             console.log(error.message);
         }
-        
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(getCurrentPosition, positionFailure);
         }
-        //$scope.location = { lat: 15.22, lng: -89.88 };
-
-        //$scope.map = {
-        //    center: {
-        //        latitude: $scope.location.lat,
-        //        longitude: $scope.location.lng
-        //    },
-        //    zoom: 12
-        //};
     }]);
