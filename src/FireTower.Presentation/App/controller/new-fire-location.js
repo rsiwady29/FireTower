6aeaa9cdea6ad8fcd8837ee94f2b50a5251d4951
unknown <fabrizio.dfc2@gmail.com>
2014-04-03 18:14:18 -0600

    Getting location from GPS/browser

--- src/FireTower.Presentation/App/controller/new-fire-location.js
+++ src/FireTower.Presentation/App/controller/new-fire-location.js
@@ -1,12 +1,38 @@
 ﻿angular.module('firetower')
     .controller('NewFireLocationController', ['$scope', '$stateParams', 'data', function ($scope, $stateParams, data) {
-        $scope.location = { lat: 15.22, lng: -89.88 };
-
+        $scope.location = { latitude: 15.22, longitude: -89.88 };
+        
         $scope.map = {
-            center: {
-                latitude: $scope.location.lat,
-                longitude: $scope.location.lng
-            },
+            center: $scope.location,
             zoom: 12
         };
+        
+        function getCurrentPosition(position) {
+            $scope.location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            };
+            
+            $scope.map = {
+                center: $scope.location,
+                zoom: 12
+            };
+        }
+        
+        function positionFailure(error) {
+            console.log(error.message);
+        }
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getCurrentPosition, positionFailure);
+        }
+        //$scope.location = { lat: 15.22, lng: -89.88 };
+
+        //$scope.map = {
+        //    center: {
+        //        latitude: $scope.location.lat,
+        //        longitude: $scope.location.lng
+        //    },
+        //    zoom: 12
+        //};
     }]);
