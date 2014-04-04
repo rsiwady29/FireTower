23392cea58a99531013cd82d80bedb09930ac96d
Richard Siwady <richard@acklenavenue.com>
2014-04-04 08:29:19 -0600

    deleted cordova platforms (ios, android)

--- src/FireTower.Presentation/App/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/richardsiwady/Desktop/FireTowerIonic/FireTower/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/richardsiwady/Desktop/FireTowerIonic/FireTower/platforms/android/AndroidManifest.xml \
