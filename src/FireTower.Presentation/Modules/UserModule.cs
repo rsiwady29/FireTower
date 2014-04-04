01a39ca4707f2dfce928d8577edc610559f407a9
Locho <pamelocho@gmail.com>
2014-04-03 19:23:57 -0600

    adding usermodule again

---
+++ src/FireTower.Presentation/Modules/UserModule.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using Nancy;
+using FireTower.Data;
+using FireTower.Domain;
+using FireTower.Domain.Entities;
+using FireTower.Infrastructure;
+using FireTower.Presentation.Responses;
+
+namespace FireTower.Presentation.Modules
+{
+    public class UserModule : NancyModule
+    {
+        public UserModule(IMappingEngine mappingEngine, IReadOnlyRepository readOnlyRepository)
+        {
+            Get["/me"] = r => mappingEngine.Map<User, MeResponse>(this.UserSession().User);
+
+            Get["/user/exists"] =
+                r =>
+                {
+                    bool exists = false;
+                    bool activated = false;
+                    var facebookid = (long)Request.Query.facebookid;
+                    try
+                    {
+                        var user =
+                            readOnlyRepository.First<User>(x => x.FacebookId == facebookid);
+                        if (user != null)
+                        {
+                            exists = true;
+                            activated = user.Verified;
+                        }
+
+                    }
+                    catch (ItemNotFoundException<User>)
+                    {
+                    }
+                    return new UserExistenceResponse
+                    {
+                        Exists = exists,
+                        Activated = activated,
+                    };
+                };
+        }
+    }
+}
